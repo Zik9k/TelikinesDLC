c2b8572125765cd61cbf8494d7a30f34cbb7d7d9
Zik9k <zirov6411@gmail.com>
2026-08-29 11:44:20 +0700

    Add module base class

---
+++ src/main/java/com/zik9k/client/Module.java
@@ -0,0 +1,39 @@
+package com.zik9k.client;
+
+public abstract class Module {
+    private final String name;
+    private final String description;
+    private final ModuleCategory category;
+    private boolean enabled;
+
+    protected Module(String name, String description, ModuleCategory category) {
+        this.name = name;
+        this.description = description;
+        this.category = category;
+    }
+
+    public final void setEnabled(boolean enabled) {
+        if (this.enabled == enabled) return;
+        this.enabled = enabled;
+        if (enabled) onEnable();
+        else onDisable();
+    }
+
+    public final void toggle() {
+        setEnabled(!enabled);
+    }
+
+    public void onTick() {
+    }
+
+    protected void onEnable() {
+    }
+
+    protected void onDisable() {
+    }
+
+    public final String getName() { return name; }
+    public final String getDescription() { return description; }
+    public final ModuleCategory getCategory() { return category; }
+    public final boolean isEnabled() { return enabled; }
+}
